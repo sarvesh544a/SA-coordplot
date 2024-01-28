f00a431bcaa805313e98f60cf15a5a23d1e30c35
sarvesh544a <sarvesh.agrawal1810@gmail.com>
2024-01-28 16:32:09 -0500

    Update MainActivity.kt
    Modified the code to include a Plotter and two sliders one for X-Axis and another for Y-Axis. Changes in the Slider results in the plotting point to move on the plotter surface.
    
    Constants like size of Outer box and inner box and offset value of -18 is defined as variables in the Column Composable.
    
    Function Map() is not using xPercent and yPercent, function Map() was already declared in the starter project with these parameters and modifier. So I created modifier variable while calling the function itself.
    
    Modified the inner-box size to 20 to make it look better.
    
    Horizontally aligned the Plotter Box to the Center of the Column.

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -3,12 +3,26 @@ package com.kodeco.android.coordplot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,12 +42,33 @@ fun PlotSurface() {
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
-        // TODO Build out the plot surface
-        //  This should include a Column composable that
-        //  includes a Map, and two MapSlider composables
-        //  (one slider for each axis).
-    }
+        Column (modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally)
+            {
+            var xPercentage: Float by remember { mutableFloatStateOf(0.5f) }
+            var yPercentage: Float by remember { mutableFloatStateOf(0.5f) }
+            val boxSize = 300
+            val innerBoxSize = 20
+            val xOffset = 10
+            val yOffset = 10
 
+            Map(xPercentage, yPercentage, modifier = Modifier.size(boxSize.dp)
+                .background(Color.LightGray)
+                .offset((xPercentage * boxSize - xOffset).dp, (yPercentage * boxSize - yOffset).dp),
+                innerBoxSize)
+
+            Slider(
+                value = xPercentage,
+                onValueChange = { xPercentage = it }
+            )
+
+            Slider(
+                value = yPercentage,
+                onValueChange = { yPercentage = it }
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
@@ -45,17 +80,15 @@ fun PlotSurfacePreview() {
 }
 
 @Composable
-fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
-    // TODO fill out the square map here.
-    //  Create a Box composable with a size of 300.dp
-    //  and then create an inner Box composable
-    //  with a clip shape of CircleShape.
+fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier, innerBoxSize: Int) {
+    Box(modifier = modifier)
+        {
+            Box(Modifier
+                .size(innerBoxSize.dp)
+                .clip(shape = CircleShape)
+                .background(Color.Red)
+            )
+        }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun MapPreview() {
-    MyApplicationTheme {
-        Map(xPercent = 0.5f, yPercent = 0.5f)
-    }
-}
+
